2835816d7d18c846778c35081d0fe8b1fcbbf57c
theusousa <137099456+theusousa@users.noreply.github.com>
2025-06-03 19:30:26 -0300

    Add files via upload

---
+++ src/cabecalho.java
@@ -0,0 +1,9 @@
+public class cabecalho {
+    public static void exibirInformacoes() {
+        System.out.println("Faculdade: [UNIFAN]");
+        System.out.println("Aluno: [MATHEUS RENAN SOUSA VELOSO]");
+        System.out.println("Professor: [BRENOR]");
+        System.out.println("Tema das Perguntas: [Tema Livre]");
+        System.out.println("---------------------------------------------------");
+    }
+}
