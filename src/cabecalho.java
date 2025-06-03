9cce0d80c5853602c32427dd3396e939f9beb2e2
theusousa <137099456+theusousa@users.noreply.github.com>
2025-06-03 19:28:05 -0300

    Delete src directory

--- src/cabecalho.java
+++
@@ -1,9 +0,0 @@
-public class cabecalho {
-    public static void exibirInformacoes() {
-        System.out.println("Faculdade: [UNIFAN]");
-        System.out.println("Aluno: [MATHEUS RENAN SOUSA VELOSO]");
-        System.out.println("Professor: [BRENOR]");
-        System.out.println("Tema das Perguntas: [Tema Livre]");
-        System.out.println("---------------------------------------------------");
-    }
-}
